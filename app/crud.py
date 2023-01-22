8bfa5d0f62ffef54505ba214eb09e104726e55b0
abdullahm1 <abdullahm@phelix.ai>
2023-01-22 14:58:11 +0500

    moving get_session() to main.py on_startup()

--- app/crud.py
+++ app/crud.py
@@ -1,11 +1,9 @@
-from cassandra.cqlengine.management import sync_table
+import copy
 from.db import get_session
 from .models import Product, ProductScrapeEvent
 import uuid
 
-session = get_session()
-sync_table(Product)
-sync_table(ProductScrapeEvent)
+
 
 def create_entry(data: dict):
     return Product.create(**data)
@@ -14,7 +12,9 @@ def create_scrape_entry(data: dict):
     data['uuid'] = uuid.uuid1() # includes a timestamp
     return ProductScrapeEvent.create(**data)
 
-def add_scrape_event(data: dict):
+def add_scrape_event(data: dict, fresh=False):
+    if fresh:
+        data = copy.deepcopy(data)
     product = create_entry(data)
     scrape_obj = create_scrape_entry(data)
     return product, scrape_obj
