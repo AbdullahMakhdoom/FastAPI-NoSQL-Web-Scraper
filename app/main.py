0475de7f73c7655cf8d862dc2599bca35ac104a7
abdullahm1 <abdullahm@phelix.ai>
2023-01-22 14:53:23 +0500

    adding notebooks_checkpoints in gitignore

---
+++ app/main.py
@@ -0,0 +1,43 @@
+from typing import List
+from fastapi import FastAPI
+
+from cassandra.cqlengine.management import sync_table
+from . import db, models, schema, config
+
+settings = config.get_settings()
+Product = models.Product
+ProductScrapeEvent = models.ProductScrapeEvent
+
+
+app = FastAPI()
+
+@app.get("/")
+def read_index():
+    return {"hello": "world", "name": settings.name}
+
+@app.get("/products", response_model=List[schema.ProductListSchema])
+def products_list_view():
+    return list(Product.objects().all())
+
+@app.get("/products/{asin}")
+def products_detail_view(asin):
+    data = dict(Product.objects().get(asin=asin))
+    events =  list(ProductScrapeEvent.objects().filter(asin=asin).limit(5))
+    data['event'] = [schema.ProductScrapeEventDetailSchema(**x) for x in events]
+    data['event_url'] = f"/products/{asin}/events" 
+    return data
+
+@app.get("/products/{asin}/events" , response_model=List[schema.ProductScrapeEventDetailSchema])
+def products_scrapes_list_view(asin):
+    return list(ProductScrapeEvent.objects().filter(asin=asin))
+
+
+session = None
+@app.on_event("startup")
+def on_startup():
+    global session
+    session = db.get_session()
+    sync_table(models.Product)
+    sync_table(models.ProductScrapeEvent)
+
+
